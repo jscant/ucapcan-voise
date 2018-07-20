b7695d3bf3cfcd8b36ba3c0f7b8f44eed9842a22
Jack Scantlebury <jack.scantlebury@gmail.com>
2018-07-20 12:00:25 +0100

    Put all mex includes in one header file

---
+++ cpp/src/mex/mexIncludes.h
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * @brief All includes for MEX functions. All files here have include guards.
+ */
+
+#ifdef MATLAB_MEX_FILE
+#include <mex.h>
+#include <matrix.h>
+#endif
+
+#include <eigen3/Eigen/Dense>
+#include <map>
+#include <functional>
+#include <string>
+
+#include "grabVD.h"
+#include "pushVD.h"
+#include "grabW.h"
+
+#include "../vd.h"
+#include "../addSeed.h"
+#include "../skizException.h"
+#include "../getRegion.h"
+#include "../typedefs.cpp"
+#include "../getCentroid.h"
+#include "../getOp.h"
+#include "../removeSeed.h"
+
+#include "../aux-functions/metrics.h"
