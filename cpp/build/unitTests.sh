5183055d480c590d08027ab1488c3cbd023577b7
Jack Scantlebury <jack.scantlebury@gmail.com>
2018-07-20 12:26:51 +0100

    Commit before going back to remove large text files

--- cpp/build/unitTests.sh
+++ cpp/build/unitTests.sh
@@ -1,11 +1,29 @@
+#!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"
+echo GetVDOpCheck:
 $DIR/GetVDOpCheck
+
+echo InVectorCheck:
 $DIR/InVectorCheck
+
+echo MetricChecks:
 $DIR/MetricChecks
+
+echo PoitnInRegionCheck:
 $DIR/PointInRegionCheck
+
+echo SqDistanceCheck:
 $DIR/SqDistanceCheck
+
+echo testSddSeedCheckLambda:
 $DIR/testAddSeedCheckLambda
+
+echo testAddSeedCheckV:
 $DIR/testAddSeedCheckV
+
+echo testRemoveSeedCheckLambda:
 $DIR/testRemoveSeedCheckLambda
+
+echo testRemoveSeedCheckV:
 $DIR/testRemoveSeedCheckV
